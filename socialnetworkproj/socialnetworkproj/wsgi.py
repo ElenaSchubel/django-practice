19bceab8e2a60743e01a79c1f5c37685f1934ba6
elena <ebassist@live.com>
2019-03-06 11:57:34 +1300

    adding proj folders

--- socialnetworkproj/socialnetworkproj/wsgi.py
+++ socialnetworkproj/socialnetworkproj/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for learning_templates project.
+WSGI config for socialnetworkproj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_templates.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socialnetworkproj.settings")
 
 application = get_wsgi_application()
